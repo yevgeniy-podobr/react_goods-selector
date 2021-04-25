bdbb4a11f36bda74d76071616a65dc19cda126e9
Yevgeniy Podobreev <podobr_2018@ukr.net>
2021-04-25 18:22:12 +0300

    Updates

---
+++ precache-manifest.47a3316c15acfb47fdc4a70f5c97050c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97c590050afe9d6ace624a8823153e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af3ee62e5d3cffb88e85",
+    "url": "./static/css/main.180011e7.chunk.css"
+  },
+  {
+    "revision": "eb5da0c87693e2e3c7c5",
+    "url": "./static/js/2.83fca28f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.83fca28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3ee62e5d3cffb88e85",
+    "url": "./static/js/main.d6b20d2e.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
